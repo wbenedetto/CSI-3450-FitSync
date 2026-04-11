75a84bd674b7352ab59f3ec7a80524f3ed0f08a1
Bryce Benedetto <bryce.benedetto29@gmail.com>
2026-04-10 22:10:01 -0400

    Started transitioning to full python/html interface to replace old JS mockup. This commit recreates the login page

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import os
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from mysql.connector import pooling
 from dotenv import load_dotenv
+from fastapi.responses import RedirectResponse
 
 # Load Environment Variables
 load_dotenv()
@@ -26,24 +27,46 @@ def get_db_conn():
     """Retrieves a connection from the connection pool."""
     return db_pool.get_connection()
 
+# @app.get("/")
+# def dashboard(request: Request):
+#     """
+#     Renders a very simple test page.
+#     Fetches all Member records from the database.
+#     """
+#     conn = get_db_conn()
+#     cursor = conn.cursor(dictionary=True)
+
+#     try:
+#         cursor.execute("SELECT * FROM Member")
+#         members = cursor.fetchall()
+
+#         return templates.TemplateResponse(
+#             request=request,
+#             name="dashboard.html", #TEST HTML FILE - CHANGE THIS TO (index.html) LATER
+#             context={"members": members}
+#         )
+#     finally:
+#         cursor.close()
+#         conn.close()
+
 @app.get("/")
-def dashboard(request: Request):
-    """
-    Renders a very simple test page.
-    Fetches all Member records from the database.
-    """
-    conn = get_db_conn()
-    cursor = conn.cursor(dictionary=True)
+def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="login.html",
+        context={}
+    )
 
-    try:
-        cursor.execute("SELECT * FROM Member")
-        members = cursor.fetchall()
+@app.post("/login")
+def login(role: str = Form(...)):
+    if role == "member":
+        return RedirectResponse(url="/member", status_code=303)
+    return RedirectResponse(url="/employee", status_code=303)
 
-        return templates.TemplateResponse(
-            request=request,
-            name="test.html", #TEST HTML FILE - CHANGE THIS TO (index.html) LATER
-            context={"members": members}
-        )
-    finally:
-        cursor.close()
-        conn.close()
+@app.get("/member")
+def member_placeholder():
+    return {"detail": "Member page not built yet"}
+
+@app.get("/employee")
+def employee_placeholder():
+    return {"detail": "Employee page not built yet"}
